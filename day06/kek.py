17d2d31db112a5366d7a1803138fcd09d2c73ba1
Josef Frola <xfrola@fi.muni.cz>
2020-12-06 13:12:45 +0100

    Day 06 solution

---
+++ day06/kek.py
@@ -0,0 +1,25 @@
+# load whole groups
+with open("input.txt") as f:
+    ls = f.read().split('\n\n')
+
+#print(ls)
+
+def question_count():
+    counter = 0
+    for l in ls:
+        counter += len(set(''.join(l.split())))
+    return counter
+     
+
+def question_count2():
+    counter = 0
+    for l in ls:
+        ppl = [set(ppl) for ppl in l.split()]
+        #print(ppl)
+    
+        counter += len(ppl[0].intersection(*ppl[1:]))
+    return counter
+
+
+print(question_count())
+print(question_count2())
